7c18eac3a153973914fc6451858f3d3dc7e9df96
Robogoogol <29505620+Robogoogol@users.noreply.github.com>
2019-10-11 17:58:08 -0400

    Add constants class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode;
+
+/**
+ * A collection of constants about the field, our robot, and Vuforia.
+ * They are primarily durations, distances, and angles.
+ */
+public class Constants {
+    /**
+     * The duration of autonomous (in milliseconds).
+     */
+    public static final long AUTONOMOUS_DURATION = 30000;
+}
