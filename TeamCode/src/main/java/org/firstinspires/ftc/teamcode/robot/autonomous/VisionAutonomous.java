c7b168a9a3482aea62f94b9978d757ba0231a579
Robogoogol <29505620+Robogoogol@users.noreply.github.com>
2019-11-10 15:56:38 -0500

    Format VisionAutonomous

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/autonomous/VisionAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/autonomous/VisionAutonomous.java
@@ -26,19 +26,21 @@ import static org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocaliz
  * An autonomous which uses vision detection
  */
 @Autonomous(name = "Vision Autonomous", group = "Robot Autonomous")
-//@Disabled
+// @Disabled
 public class VisionAutonomous extends LinearOpMode {
     /**
      * The camera direction
      */
     private static final VuforiaLocalizer.CameraDirection CAMERA_CHOICE = BACK;
+    /**
+     * Whether or not the phone is in portrait mode.
+     */
     private static final boolean PHONE_IS_PORTRAIT = false;
 
     /**
      * Vuforia license key.
      */
-    private static final String VUFORIA_KEY =
-            "AagmIFb/////AAABmQeoELJRUUhBnw22N1rfAeURKsi4lO2PBquW4po2++umNgidlnmVALYdHmMXwjhAD9owXoF2zkCbWmBDEStv642zdEddYZGqPjK2pn4bDvhEeSVj4mQs3zR7mB2T94RenHo+qz8zhq4yidyNRZNYF/Y3OUTayx7H6EtYnU6kaOZi30xW6ZzrzzyP/dEG5mHV2pzBHTEu/Qe1g7RcsCG5sFDg0KAagyjxgC6X3z4/EA0tC2554q+o8S+glD7nFidnAF5e2Pti3+gAnwDN6Nl/nRGrsD0JyAVUFFk2Ii0uEorRz31VCD3C4+ib2UHN0QOZwTYgqXQ4JcH5bXJsqn1aDmSeky2smnk7xamtfXkdBm0I";
+    private static final String VUFORIA_KEY = "AagmIFb/////AAABmQeoELJRUUhBnw22N1rfAeURKsi4lO2PBquW4po2++umNgidlnmVALYdHmMXwjhAD9owXoF2zkCbWmBDEStv642zdEddYZGqPjK2pn4bDvhEeSVj4mQs3zR7mB2T94RenHo+qz8zhq4yidyNRZNYF/Y3OUTayx7H6EtYnU6kaOZi30xW6ZzrzzyP/dEG5mHV2pzBHTEu/Qe1g7RcsCG5sFDg0KAagyjxgC6X3z4/EA0tC2554q+o8S+glD7nFidnAF5e2Pti3+gAnwDN6Nl/nRGrsD0JyAVUFFk2Ii0uEorRz31VCD3C4+ib2UHN0QOZwTYgqXQ4JcH5bXJsqn1aDmSeky2smnk7xamtfXkdBm0I";
 
     /**
      * The amount of millimeters in an inch.
@@ -127,13 +129,9 @@ public class VisionAutonomous extends LinearOpMode {
         webcamName = hardwareMap.get(WebcamName.class, "Webcam 1");
 
         // Create the Vuforia parameters using the camera ID.
-        int cameraMonitorViewId = hardwareMap.appContext.getResources()
-                .getIdentifier(
-                        "cameraMonitorViewId", "id",
-                        hardwareMap.appContext.getPackageName()
-                );
-        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer
-                .Parameters(cameraMonitorViewId);
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id",
+                hardwareMap.appContext.getPackageName());
+        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
 
         // Set the Vuforia license key.
         parameters.vuforiaLicenseKey = VUFORIA_KEY;
@@ -145,8 +143,7 @@ public class VisionAutonomous extends LinearOpMode {
         vuforia = ClassFactory.getInstance().createVuforia(parameters);
 
         // Load the Skystone targets.
-        VuforiaTrackables skystoneTargets = this.vuforia
-                .loadTrackablesFromAsset("Skystone");
+        VuforiaTrackables skystoneTargets = this.vuforia.loadTrackablesFromAsset("Skystone");
 
         // Stone.
         VuforiaTrackable stoneTarget = skystoneTargets.get(0);
@@ -191,79 +188,76 @@ public class VisionAutonomous extends LinearOpMode {
         // Create list of trackables.
         List<VuforiaTrackable> allTrackables = new ArrayList<VuforiaTrackable>(skystoneTargets);
 
-        // Set the position of the Stone Target. Since it's not fixed in position, assume it's at
-        // the field origin. Rotated it to to face forward, and raised it to sit on the ground
+        // Set the position of the Stone Target. Since it's not fixed in position,
+        // assume it's at
+        // the field origin. Rotated it to to face forward, and raised it to sit on the
+        // ground
         // correctly. This can be used for generic target-centric approach algorithms.
-        stoneTarget.setLocation(OpenGLMatrix
-                .translation(0, 0, stoneZ)
+        stoneTarget.setLocation(OpenGLMatrix.translation(0, 0, stoneZ)
                 .multiplied(Orientation.getRotationMatrix(EXTRINSIC, XYZ, DEGREES, 90, 0, -90)));
 
-        // Set the position of the bridge support targets with relation to the origin (center of the
+        // Set the position of the bridge support targets with relation to the origin
+        // (center of the
         // field).
-        blueFrontBridge.setLocation(OpenGLMatrix
-                .translation(-bridgeX, bridgeY, bridgeZ)
+        blueFrontBridge.setLocation(OpenGLMatrix.translation(-bridgeX, bridgeY, bridgeZ)
                 .multiplied(Orientation.getRotationMatrix(EXTRINSIC, XYZ, DEGREES, 0, bridgeRotY, bridgeRotZ)));
 
-        blueRearBridge.setLocation(OpenGLMatrix
-                .translation(-bridgeX, bridgeY, bridgeZ)
+        blueRearBridge.setLocation(OpenGLMatrix.translation(-bridgeX, bridgeY, bridgeZ)
                 .multiplied(Orientation.getRotationMatrix(EXTRINSIC, XYZ, DEGREES, 0, -bridgeRotY, bridgeRotZ)));
 
-        redFrontBridge.setLocation(OpenGLMatrix
-                .translation(-bridgeX, -bridgeY, bridgeZ)
+        redFrontBridge.setLocation(OpenGLMatrix.translation(-bridgeX, -bridgeY, bridgeZ)
                 .multiplied(Orientation.getRotationMatrix(EXTRINSIC, XYZ, DEGREES, 0, -bridgeRotY, 0)));
 
-        redRearBridge.setLocation(OpenGLMatrix
-                .translation(bridgeX, -bridgeY, bridgeZ)
+        redRearBridge.setLocation(OpenGLMatrix.translation(bridgeX, -bridgeY, bridgeZ)
                 .multiplied(Orientation.getRotationMatrix(EXTRINSIC, XYZ, DEGREES, 0, bridgeRotY, 0)));
 
-        // Set the position of the perimeter targets with relation to the origin (center of the field).
-        red1.setLocation(OpenGLMatrix
-                .translation(quadField, -halfField, mmTargetHeight)
+        // Set the position of the perimeter targets with relation to the origin (center
+        // of the field).
+        red1.setLocation(OpenGLMatrix.translation(quadField, -halfField, mmTargetHeight)
                 .multiplied(Orientation.getRotationMatrix(EXTRINSIC, XYZ, DEGREES, 90, 0, 180)));
 
-        red2.setLocation(OpenGLMatrix
-                .translation(-quadField, -halfField, mmTargetHeight)
+        red2.setLocation(OpenGLMatrix.translation(-quadField, -halfField, mmTargetHeight)
                 .multiplied(Orientation.getRotationMatrix(EXTRINSIC, XYZ, DEGREES, 90, 0, 180)));
 
-        front1.setLocation(OpenGLMatrix
-                .translation(-halfField, -quadField, mmTargetHeight)
+        front1.setLocation(OpenGLMatrix.translation(-halfField, -quadField, mmTargetHeight)
                 .multiplied(Orientation.getRotationMatrix(EXTRINSIC, XYZ, DEGREES, 90, 0, 90)));
 
-        front2.setLocation(OpenGLMatrix
-                .translation(-halfField, quadField, mmTargetHeight)
+        front2.setLocation(OpenGLMatrix.translation(-halfField, quadField, mmTargetHeight)
                 .multiplied(Orientation.getRotationMatrix(EXTRINSIC, XYZ, DEGREES, 90, 0, 90)));
 
-        blue1.setLocation(OpenGLMatrix
-                .translation(-quadField, halfField, mmTargetHeight)
+        blue1.setLocation(OpenGLMatrix.translation(-quadField, halfField, mmTargetHeight)
                 .multiplied(Orientation.getRotationMatrix(EXTRINSIC, XYZ, DEGREES, 90, 0, 0)));
 
-        blue2.setLocation(OpenGLMatrix
-                .translation(quadField, halfField, mmTargetHeight)
+        blue2.setLocation(OpenGLMatrix.translation(quadField, halfField, mmTargetHeight)
                 .multiplied(Orientation.getRotationMatrix(EXTRINSIC, XYZ, DEGREES, 90, 0, 0)));
 
-        rear1.setLocation(OpenGLMatrix
-                .translation(halfField, quadField, mmTargetHeight)
+        rear1.setLocation(OpenGLMatrix.translation(halfField, quadField, mmTargetHeight)
                 .multiplied(Orientation.getRotationMatrix(EXTRINSIC, XYZ, DEGREES, 90, 0, -90)));
 
-        rear2.setLocation(OpenGLMatrix
-                .translation(halfField, -quadField, mmTargetHeight)
+        rear2.setLocation(OpenGLMatrix.translation(halfField, -quadField, mmTargetHeight)
                 .multiplied(Orientation.getRotationMatrix(EXTRINSIC, XYZ, DEGREES, 90, 0, -90)));
 
         // Create a transformation matrix describing where the phone is on the robot.
         //
-        // NOTE: It's very important that you turn *off* your phone's automatic screen rotation
+        // NOTE: It's very important that you turn *off* your phone's automatic screen
+        // rotation
         // option. Lock it into portrait mode for these numbers to work.
         //
         // NOTE: The coordinate frame for the robot looks the same as the field.
-        // The robot's forward direction is facing out along X axis, with the LEFT side facing out
-        // along the Y axis. Z is UP on the robot. This equates to a bearing angle of 0 degrees.
+        // The robot's forward direction is facing out along X axis, with the LEFT side
+        // facing out
+        // along the Y axis. Z is UP on the robot. This equates to a bearing angle of 0
+        // degrees.
         //
-        // The phone starts out lying flat, with the screen facing up and with the physical top of
+        // The phone starts out lying flat, with the screen facing up and with the
+        // physical top of
         // the phone pointing to the left side of the robot.
         //
-        // The two examples below assume that the camera is facing forward out the front of the robot.
+        // The two examples below assume that the camera is facing forward out the front
+        // of the robot.
 
-        // We need to rotate the camera around it's long axis to bring the correct camera forward.
+        // We need to rotate the camera around it's long axis to bring the correct
+        // camera forward.
         if (CAMERA_CHOICE == BACK) {
             phoneYRotation = -90;
         } else {
@@ -276,29 +270,26 @@ public class VisionAutonomous extends LinearOpMode {
         }
 
         // Next, translate the camera lens to where it is on the robot.
-        // In this example, it is centered (left to right), but forward of the middle of the robot,
+        // In this example, it is centered (left to right), but forward of the middle of
+        // the robot,
         // and above ground level.
 
         // e.g. camera is 4 inches in front of the robot's center.
         final float CAMERA_X_DISPLACEMENT = 4.0f * mmPerInch;
         // e.g. camera is 8 inches above the ground.
         final float CAMERA_Y_DISPLACEMENT = 8.0f * mmPerInch;
-        // e.g. camera is 0 inches to the left of the robot's center, or on its center axis.
+        // e.g. camera is 0 inches to the left of the robot's center, or on its center
+        // axis.
         final float CAMERA_Z_DISPLACEMENT = 0.0f;
 
         OpenGLMatrix robotFromCamera = OpenGLMatrix
-                .translation(CAMERA_X_DISPLACEMENT, CAMERA_Y_DISPLACEMENT, CAMERA_Z_DISPLACEMENT)
-                .multiplied(
-                        Orientation.getRotationMatrix(
-                                EXTRINSIC, YZX, DEGREES,
-                                phoneYRotation, phoneZRotation, phoneXRotation
-                        )
-                );
+                .translation(CAMERA_X_DISPLACEMENT, CAMERA_Y_DISPLACEMENT, CAMERA_Z_DISPLACEMENT).multiplied(Orientation
+                        .getRotationMatrix(EXTRINSIC, YZX, DEGREES, phoneYRotation, phoneZRotation, phoneXRotation));
 
         // Let all of the trackable listeners know where the phone is.
         for (VuforiaTrackable trackable : allTrackables) {
-            ((VuforiaTrackableDefaultListener) trackable.getListener())
-                    .setPhoneInformation(robotFromCamera, parameters.cameraDirection);
+            ((VuforiaTrackableDefaultListener) trackable.getListener()).setPhoneInformation(robotFromCamera,
+                    parameters.cameraDirection);
         }
 
         // NOTE:
@@ -325,9 +316,8 @@ public class VisionAutonomous extends LinearOpMode {
                     // getUpdatedRobotLocation() will return null if no new information is available
                     // since the last time that call was made, or if the trackable is not currently
                     // visible.
-                    OpenGLMatrix robotLocationTransform =
-                            ((VuforiaTrackableDefaultListener) trackable.getListener())
-                                    .getUpdatedRobotLocation();
+                    OpenGLMatrix robotLocationTransform = ((VuforiaTrackableDefaultListener) trackable.getListener())
+                            .getUpdatedRobotLocation();
                     if (robotLocationTransform != null) {
                         lastLocation = robotLocationTransform;
                     }
@@ -340,22 +330,13 @@ public class VisionAutonomous extends LinearOpMode {
                 // Express translation of the robot in inches.
                 VectorF translation = lastLocation.getTranslation();
                 // X, Y, and Z.
-                telemetry.addData(
-                        "Pos (in)",
-                        "(%.1f, %.1f, %.1f)",
-                        translation.get(0) / mmPerInch,
-                        translation.get(1) / mmPerInch,
-                        translation.get(2) / mmPerInch);
+                telemetry.addData("Pos (in)", "(%.1f, %.1f, %.1f)", translation.get(0) / mmPerInch,
+                        translation.get(1) / mmPerInch, translation.get(2) / mmPerInch);
 
                 // Express the rotation of the robot in degrees.
-                Orientation rotation = Orientation
-                        .getOrientation(lastLocation, EXTRINSIC, XYZ, DEGREES);
+                Orientation rotation = Orientation.getOrientation(lastLocation, EXTRINSIC, XYZ, DEGREES);
                 // Roll, pitch, and yaw.
-                telemetry.addData(
-                        "Rot (deg)",
-                        "(%.0f, %.0f, %.0f)",
-                        rotation.firstAngle,
-                        rotation.secondAngle,
+                telemetry.addData("Rot (deg)", "(%.0f, %.0f, %.0f)", rotation.firstAngle, rotation.secondAngle,
                         rotation.thirdAngle);
             } else {
                 telemetry.addData("Visible Target", "none");
