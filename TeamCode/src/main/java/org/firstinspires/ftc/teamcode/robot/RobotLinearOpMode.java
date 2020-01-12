462204c7f5d14dc3f5b77c7d52512fc8aa89fd30
jakbyte <jakbyte@gmail.com>
2020-01-11 21:03:47 -0500

    Update movement duration to skybridge

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/RobotLinearOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/RobotLinearOpMode.java
@@ -33,12 +33,12 @@ public class RobotLinearOpMode extends LinearOpMode {
     /**
      * The duration to move toward the skybridge.
      */
-    public static final long MOVEMENT_TO_SKYBRIDGE_DURATION = 1250;
+    public static final long MOVEMENT_TO_SKYBRIDGE_DURATION = 2500;
 
     /**
      * The duration to move toward the skybridge, but for taking the scenic route.
      */
-    public static final long SCENIC_MOVEMENT_TO_SKYBRIDGE_DURATION = 1000;
+    public static final long SCENIC_MOVEMENT_TO_SKYBRIDGE_DURATION = 2500;
 
     public double drivePower = 0.5;
     protected RobotHardware robot = new RobotHardware();
