05ca8e295daa217d2fb11062e179c28b445e3ff5
SenorBunBun <33770177+SenorBunBun@users.noreply.github.com>
2020-01-18 09:29:41 -0500

    WIP

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode;
+
+public class Constants {
+    private static final double FOUNDATION_MAX = 0.70;
+    private static final double FOUNDATION_MIN = 0.25;
+    private static final double LEFT_CLAW_MAX = 0.8;
+    private static final double LEFT_CLAW_MIN = 0.2;
+    private static final double RIGHT_CLAW_MAX = 1.0;
+    private static final double RIGHT_CLAW_MIN = 0.0;
+    private static final double ARM_MAX = 1.0;
+    private static final double ARM_MIN = 0.125;
+}
