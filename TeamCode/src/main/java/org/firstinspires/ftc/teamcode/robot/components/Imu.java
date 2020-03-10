968495aeacc0ba5839436bbdb163916f558e0101
jakbyte <jakbyte@gmail.com>
2020-03-10 19:30:00 -0400

    Add IMU component

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/components/Imu.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode.robot.components;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
+
+public class Imu {
+    public BNO055IMU imu;
+
+    public Imu(BNO055IMU imu) {
+        this.imu = imu;
+
+        initialize();
+    }
+
+    private void initialize() {
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.calibrationDataFile = "BN055IMUCalibration.json";
+        parameters.loggingEnabled = true;
+        parameters.loggingTag = "IMU";
+        parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
+
+        imu.initialize(parameters);
+    }
+}
